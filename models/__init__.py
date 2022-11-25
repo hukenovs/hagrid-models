9dd97d7b806eee28aec41751f745107e10e8dbd5
anotherhelloworld <helloworld106@gmail.com>
2022-11-25 16:31:29 +0500

    Remove TODO from models __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,3 @@ from .classifiers.mobilenet import *
 from .classifiers.resnet import *
 from .classifiers.vision_transformer import *
 from .detectors.ssdlite import *
-
-# TODO: add classificaion and detection models from model zoo
