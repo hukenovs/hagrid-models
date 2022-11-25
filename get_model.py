766471f47b36e167e8964ce02eee6001de9d5513
anotherhelloworld <helloworld106@gmail.com>
2022-11-25 11:55:40 +0500

    Add kwargs to get_model method.

--- get_model.py
+++ get_model.py
@@ -1,36 +1,46 @@
 import os
 from functools import singledispatch
+from typing import Any
+
+import requests
+from tqdm.auto import tqdm
+
 from hagrid_models.model_zoo import models
 from hagrid_models.onnx_model import ONNXModel
 from hagrid_models.pytorch_model import pytorch_model
-import requests
-from tqdm.auto import tqdm
 
 __all__ = ["get_model"]
 
 
-class URL(str): ...
-class PATH(str): ...
-class Name(str): ...
+class URL(str):
+    ...
+
+
+class PATH(str):
+    ...
+
+
+class Name(str):
+    ...
 
 
 @singledispatch
-def _get_model(model):
+def _get_model(model, **kwargs: Any):
     return model
 
 
 @_get_model.register
-def _(model: URL, progress: bool):
+def _(model: URL, progress: bool, **kwargs: Any):
     model_storage = os.path.join(os.path.dirname(__file__), "model_storage")
     if not os.path.exists(model_storage):
         os.mkdir(model_storage)
     response = requests.get(model, stream=True)
     url = response.url
-    file_size = int(response.headers.get('content-length', 0))
+    file_size = int(response.headers.get("content-length", 0))
     filename = os.path.join(model_storage, url.split("/")[-1])
     if os.path.exists(filename):
         return _get_model(PATH(filename))
-    progress_bar = tqdm(total=file_size, unit='iB', unit_scale=True, disable=not progress, desc=url.split('/')[-1])
+    progress_bar = tqdm(total=file_size, unit="iB", unit_scale=True, disable=not progress, desc=url.split("/")[-1])
     block_size = 1024
     with open(filename, "wb") as handle:
         for data in response.iter_content(chunk_size=block_size):
@@ -41,43 +51,42 @@ def _(model: URL, progress: bool):
     if file_size != 0 and progress_bar.n != file_size:
         raise ValueError("Error, something went wrong")
 
-    return _get_model(PATH(filename))
+    return _get_model(PATH(filename), **kwargs)
 
 
 @_get_model.register
-def _(model: PATH):
+def _(model: PATH, **kwargs: Any):
     if not os.path.exists(model):
         raise ValueError(f"Checkpoint {model} not found.")
-    if '.onnx' in model:
+    if ".onnx" in model:
         return ONNXModel(model)
-    elif '.pth' in model or '.pt' in model:
-        return pytorch_model(checkpoint=model)
+    elif ".pth" in model or ".pt" in model:
+        return pytorch_model(checkpoint=model, **kwargs)
     else:
         raise ValueError(f"Checkpoint {model} not supported.")
 
 
 @_get_model.register
-def _(model: Name, progress: bool):
+def _(model: Name, progress: bool, **kwargs: Any):
     model = models[model]
-    model = _get_model(URL(model), progress)
+    model = _get_model(URL(model), progress, **kwargs)
 
     return model
 
 
-
-def get_model(model: str, progress: bool = True, pretrained: bool = True, device: str = None, **kwargs):
+def get_model(model: str, progress: bool = True, pretrained: bool = True, device: str = None, **kwargs: Any):
     if pretrained:
         if "https" in model and model in models.values():
-            model = _get_model(URL(model), progress)
+            model = _get_model(URL(model), progress, **kwargs)
         elif ".onnx" in model or ".pth" in model or ".pt" in model:
-            model = _get_model(PATH(model))
+            model = _get_model(PATH(model), kwargs, **kwargs)
         elif model in models.keys():
-            model = _get_model(Name(model), progress)
+            model = _get_model(Name(model), progress, **kwargs)
         else:
             raise ValueError(f"Model {model} not found.")
     else:
         if model in models.keys():
-            return pytorch_model(model_name=model)
+            return pytorch_model(model_name=model, **kwargs)
         else:
             raise ValueError(f"Model {model} not found.")
 
@@ -85,5 +94,5 @@ def get_model(model: str, progress: bool = True, pretrained: bool = True, device
 
 
 if __name__ == "__main__":
-    out = get_model('SwinT_FasterRCNN')
+    out = get_model("SwinT_FasterRCNN")
     print(type(out).__name__)
--- pytorch_model.py
+++ pytorch_model.py
@@ -1,21 +1,25 @@
 import os
-from hagrid_models import models
+from typing import Any
+
 import torch
 
+from hagrid_models import models
 
-def pytorch_model(model_name: str = None, checkpoint: str = None):
+
+def pytorch_model(model_name: str = None, checkpoint: str = None, **kwargs: Any):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if model_name is None:
         if checkpoint is None:
             raise ValueError("Either model_name or checkpoint must be specified.")
-        model_name = os.path.basename(checkpoint).split('.')[0]
+        model_name = os.path.basename(checkpoint).split(".")[0]
     try:
-        model_class = getattr(models,
-                              model_name.lower())  # parse model from model_zoo, need to add raise error if model not found
+        model_class = getattr(
+            models, model_name.lower()
+        )  # parse model from model_zoo, need to add raise error if model not found
     except AttributeError:
         raise AttributeError(f"Model {model_name} not found.") from None
 
-    model = model_class()  # return model class with or without pretrained weights
+    model = model_class(**kwargs)  # return model class with or without pretrained weights
     if checkpoint is not None:
         state_dict = torch.load(checkpoint, map_location=device)
         model.load_state_dict(state_dict["state_dict"])
