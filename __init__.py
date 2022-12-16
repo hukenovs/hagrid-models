cf440cc4f243c60be2895482961cec2c69d57d51
anotherhelloworld <helloworld106@gmail.com>
2022-12-16 16:05:24 +0500

    Update __init__.py according to prev commit.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
-from .get_model import get_model
-from .onnx_model import ONNXModel
-from .pytorch_model import pytorch_model
+# from .get_model import get_model
+# from .onnx_model import ONNXModel
+# from .pytorch_model import pytorch_model
+
+from .wrappers.get_model import get_model
+from .wrappers.onnx_model import ONNXModel
+from .wrappers.pytorch_model import pytorch_model
